10cbaf5a15af3a33d32ec931866f1f68d7513e57
Mike Fanger <mfanger@nationalsystems.com>
2017-05-08 01:14:07 -0500

    Update gulpfile to build minified CSS.

--- grid/gulpfile.js
+++ grid/gulpfile.js
@@ -1,31 +1,40 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var path = require('path');
 
-// gulp.task('sass', function () {
-//   return gulp.src('./sass/**/*.scss')
-//     .pipe(sass().on('error', sass.logError))
-//     .pipe(gulp.dest('./css'));
-// });
-
-// gulp.task('sass:watch', function () {
-//   gulp.watch('./sass/**/*.scss', ['sass']);
-// });
-
-function buildSass({ src, dest }) {
+function buildSass({ src, dest, options }) {
+  options = options || {};
   return () => {
     return gulp.src(src)
-      .pipe(sass().on('error', sass.logError))
+      .pipe(rename(function(path) {
+        if(options.outputStyle === 'compressed') {
+          path.extname = '.min' + path.extname;
+        }
+      }))
+      .pipe(sass(options).on('error', sass.logError))
       .pipe(gulp.dest(dest));
   };
 }
 
-function server() {
+// paths
+var assetsDir = './public/assets/';
+var gridRoot = './sass/grid/grid.scss';
+var stylesRoot = './sass/styles/styles.scss';
 
-}
+var sassOpts = {
+  outputStyle: 'compressed',
+};
 
-gulp.task('build-grid', buildSass({ src: './sass/grid/grid.scss', dest: './public/assets/' }));
-gulp.task('build-styles', buildSass({ src: './sass/styles/styles.scss', dest: './public/assets/' }));
-gulp.task('sass:watch', [ 'build-grid', 'build-styles' ], function() {
-  gulp.watch('./sass/grid/grid.scss', [ 'build-grid' ]);
+gulp.task('build-grid', buildSass({ src: gridRoot, dest: assetsDir }));
+gulp.task('build-grid:min', buildSass({ src: gridRoot, dest: assetsDir, options: sassOpts }));
+
+gulp.task('build-styles', buildSass({ src: stylesRoot, dest: assetsDir }));
+gulp.task('build-styles:min', buildSass({ src: stylesRoot, dest: assetsDir, options: sassOpts }));
+
+gulp.task('sass:watch', [ 'build-grid', 'build-grid:min', 'build-styles', 'build-styles:min' ], function() {
+  gulp.watch('./sass/grid/**/*.scss', [ 'build-grid' ]);
   gulp.watch('./sass/styles/**/*.scss', [ 'build-styles' ]);
 });
+
+gulp.task('sass', [ 'build-grid', 'build-grid:min', 'build-styles', 'build-styles:min' ]);
